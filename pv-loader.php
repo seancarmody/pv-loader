87f6dd924cfe6cc6ed2e797838b1345b58df251a
Sean Carmody <seancarmody@gmail.com>
2010-09-08 21:25:58 +1000

    Changed indent spaces to tabs.

--- pv-loader.php
+++ pv-loader.php
@@ -31,18 +31,18 @@ Copyright 2010 Sean Carmody  (email : sean@stubbornmule.net)
 wp_enqueue_script('protovis', WP_PLUGIN_URL.'/pv-loader/js/protovis-r3.2.js');
 
 function sProtovisLoad($atts, $content = null) {
-   extract(shortcode_atts(array(
-                               'src' => '#',
-                               'img' => '#',
-   ), $atts));
-   if ( $img ) 
-       $no_script = "<noscript><img src='$img'></noscript>";
-   else
-       $no_script = '<noscript>Scripts disabled, cannot display image!</noscript>';
-   $script = file_get_contents($src);
-   $script = '<script type="text/javascript+protovis">'."\n".$script.'</script>';
-   $caption = '<p align="center"><strong>'.do_shortcode($content).'</strong></p>';
-   return $script.$no_script.$caption;
+	extract(shortcode_atts(array(
+		'src' => '#',
+		'img' => '#',
+	), $atts));
+	if ( $img )
+		$no_script = "<noscript><img src='$img'></noscript>";
+	else
+		$no_script = '<noscript>Scripts disabled, cannot display image!</noscript>';
+	$script = file_get_contents($src);
+	$script = '<script type="text/javascript+protovis">'."\n".$script.'</script>';
+	$caption = '<p align="center"><strong>'.do_shortcode($content).'</strong></p>';
+	return $script.$no_script.$caption;
 }
 add_shortcode('pvis', 'sProtovisLoad');
 
